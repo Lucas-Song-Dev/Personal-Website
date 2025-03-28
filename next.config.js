927436142d96de9b31816c56a34cc4eeb57e2e4c
Lucas Song <aosdjs@students.cs.ubc.ca>
2025-03-27 19:59:54 -0700

    set up next.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
