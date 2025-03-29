c18c33f8202d475bed9ec9edaf0b3ba35508bcc5
Lucas Song <aosdjs@students.cs.ubc.ca>
2025-03-28 18:58:43 -0700

    update drag text to be more clear

--- src/components/DragAnywhere.tsx
+++ src/components/DragAnywhere.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 export default function DragAnywhere() {
   return (
     <div className="fixed top-8 left-8 z-50 text-responsive-h6 text-text/70">
-      DRAG ANYWHERE
+      CLICK & DRAG
     </div>
   );
 }
